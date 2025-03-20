bb6827b8129a8ff30c53124101d19c41b5df7c46
atakallou <156933358+atakallou@users.noreply.github.com>
2025-03-20 12:59:29 -0500

    update

--- DA/DAFunctions.py
+++ DA/DAFunctions.py
@@ -111,3 +111,50 @@ def model_rout (res_path, parms, initialPath = None , stopdate = None):
     #                                     f"{sim['YEAR'].iat[-1]}{str(sim['MONTH'].iat[-1]).zfill(2)}{str(sim['DAY'].iat[-1]).zfill(2)}.npy")
     #     np.save(rout_state_path, rout_states)
     return Qflow, rout_states
+    
+def Cleaner(Master_folder):
+    for ens in range(0,100):
+        directory =  os.path.join(Master_folder, f"Ens_{ens}", "results")
+        for file in os.listdir(directory):
+            file_path = os.path.join(directory, file)
+            os.remove(file_path)
+    for ens in range(0,100):
+        directory =  os.path.join(Master_folder, f"Ens_{ens}", "States")
+        for file in os.listdir(directory):
+            file_path = os.path.join(directory, file)
+            os.remove(file_path)
+
+def read_state(Ens_folder,date):
+    year, month, day = str(date.year).zfill(4), str(date.month).zfill(2), str(date.day).zfill(2)
+    state_file_path = os.path.join(Ens_folder, "States", f"States_{year}{month}{day}_00000")
+    rout_file_path = os.path.join(Ens_folder, "results", f"Rout_{year}{month}{day}.npy")
+    rout_states      = np.load(rout_file_path)
+    with open(state_file_path, "r") as f:
+        lines = f.readlines()
+    state_line = lines[3].split(" ")
+    states = np.array([float(state_line[2]), float(state_line[3]), float(state_line[4]), float(state_line[12]), rout_states[0], rout_states[1], rout_states[2]])
+    return states
+
+def write_state(Ens_folder,date, states):
+    year, month, day = str(date.year).zfill(4), str(date.month).zfill(2), str(date.day).zfill(2)
+    state_file_path = os.path.join(Ens_folder, "States", f"States_{year}{month}{day}_00000")
+    rout_file_path = os.path.join(Ens_folder, "results", f"Rout_{year}{month}{day}.npy")
+    rout_states      = np.load(rout_file_path)
+    with open(state_file_path, "r") as f:
+        lines = f.readlines()
+    state_line = lines[3].split(" ")
+    state_line[2] = str(states[0])
+    state_line[3] = str(states[1])
+    state_line[4] = str(states[2])
+    state_line[12] = str(states[3])
+    rout_states[0]= states[4]
+    rout_states[1]= states[5]
+    rout_states[2]= states[6]
+    lines[3] = " ".join(state_line) 
+    if len(lines) > 5:
+        if float(lines[4].split(" ")[2]) > 0.0:
+            lines[4] = " ".join(state_line) 
+    with open(state_file_path, "w") as f:
+        f.writelines(lines)
+    np.save(rout_file_path , rout_states)
+    return 1
